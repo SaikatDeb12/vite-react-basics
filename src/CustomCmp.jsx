03c7170b88e36ac500e147e2d7c206ff6042d5b9
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-04 01:48:49 +0530

    creating a component

---
+++ src/CustomCmp.jsx
@@ -0,0 +1,9 @@
+function CustomCmp(){
+    //for some libraries if a function is returning jsx,
+    //name the file with jsx
+    return (
+        <h2>This is new component</h2>
+    );
+}
+
+export default CustomCmp;
